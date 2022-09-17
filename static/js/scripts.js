a16708a2183b88adb1cc7003da9fbf9a9ba2740c
Diana Avila <missernightangel@gmail.com>
2022-09-16 22:16:04 -0300

    up

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,3 +2,8 @@ function change(){
   document.body.classList.toggle('dark-theme');
   document.body.classList.toggle('light-theme');
 }
+
+function whatsapp() {
+  let send = document.querySelector("#whatsapp");
+  send.setAttribute('href', 'https://wa.me/5516981994052');
+}
